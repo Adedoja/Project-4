b8a55a4adb748cd07156749d25a1d6ecdf803065
sarahjprog <132375086+sarahjprog@users.noreply.github.com>
2025-12-01 10:48:11 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,3 +3,26 @@
 #include <string.h>
 #include <ctype.h>
 
+int main(int argc, char *argv[]){
+    FILE *intermediate=NULL;
+    FILE *fp=NULL;
+    FILE *object=NULL;
+    int returnvalue=0;
+
+    if (argc <2){
+        printf("error, there should be two arguments %s <sourcefile>\n", argv[0]);
+        return 1;
+    }
+    const char *Source= argv[1];
+    char ObjectFileName[256];
+    char intermediate_file[256];
+
+    sprintf(intermediate_file,  "%.*s.int", (int)(strlen(Source) -4), Source);
+    sprintf(ObjectFileName, "%s.obj", Source );
+
+
+    fp= fopen(Source, "r" );
+    if (!fp){
+       printf("error: cannot open input file %s\n", argv[1]);
+       return 1;
+    }
